90127107b840b5d060005b15778246f55fd21ac6
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-08-31 16:46:37 -0400

    feat: add test method

--- src/main/java/com/jeremy/App.java
+++ src/main/java/com/jeremy/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Super cool repo" );
+        testMethod();
+    }
+
+    /**
+     * This is a test method
+     */
+    public static void testMethod() {
+        System.out.println("This is a test method");
     }
 }
